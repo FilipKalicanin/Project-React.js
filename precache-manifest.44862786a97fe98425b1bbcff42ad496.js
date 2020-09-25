bbd3d8f86432d1f0ce11090e64c091822c23014a
FilipKalicanin <filipkalicanin@hotmail.com>
2020-09-25 10:19:59 +0200

    Updates

---
+++ precache-manifest.44862786a97fe98425b1bbcff42ad496.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c5aa925712df61c21858a101776f5b",
+    "url": "/Project-React.js/index.html"
+  },
+  {
+    "revision": "2b9d263effff9491f290",
+    "url": "/Project-React.js/static/css/2.70e35f80.chunk.css"
+  },
+  {
+    "revision": "a350f99002591241adaa",
+    "url": "/Project-React.js/static/css/main.17fc231a.chunk.css"
+  },
+  {
+    "revision": "2b9d263effff9491f290",
+    "url": "/Project-React.js/static/js/2.e7d55b18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Project-React.js/static/js/2.e7d55b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a350f99002591241adaa",
+    "url": "/Project-React.js/static/js/main.b6124fba.chunk.js"
+  },
+  {
+    "revision": "5f42aaf6dae8e0ba0d0c",
+    "url": "/Project-React.js/static/js/runtime-main.50e07e05.js"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/Project-React.js/static/media/Lobster-Regular.9406d0ab.ttf"
+  }
+]);
